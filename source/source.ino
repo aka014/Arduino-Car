44ed953ed9119b28b1a77643f2bef5716d8d30e1
aljosa <aljosa.crnogorac@gmail.com>
2025-05-25 14:33:27 +0200

    Add emergency braking, still no 12 V voltage

--- source/source.ino
+++ source/source.ino
@@ -2,6 +2,10 @@
 
 // IR receiver pin
 const int RECV_PIN = 2;
+// Ultrasonic distance sensor Trigger pin
+const int TRIG_PIN = 12;
+// Ultrasonic distance sensor Echo pin
+const int ECHO_PIN = 13;
 // Light intensity sensitive LED pin
 // THIS PIN MIGHT NOT WORK FOR PWM RIGHT IF MILLIS() OR DELAY() IS USED!
 const int LED_PIN = 6;
@@ -19,6 +23,7 @@ const int ENA = 9;
 // Start motor B (right wheels)
 const int ENB = 10;
 
+
 // IR remote controller button 2
 const int REMOTE_NUM_2 = 0x18;
 // IR remote controller button 4
@@ -31,24 +36,27 @@ const int REMOTE_NUM_7 = 0x42;
 const int REMOTE_NUM_8 = 0x52;
 // IR remote controller button 9
 const int REMOTE_NUM_9 = 0x4A;
+// Joystick neutral position
+const int STOP = 0x20;
 
 // Used to measure time elapsed from the last LED brightness update
 int lastTick = 0;
 
 // Delay duration
-const int TICK = 50;
+const int TICK = 127;
 // If photoresistor's voltage is below this threshold, LED turns on (Note: Voltage is read in units where a unit = 4.9 mV)
 const int BR_THRESHOLD = 300;
+// Minimum allowed distance from obstacle while going backward
+const int DIST_THRESHOLD = 20;
 
 // Create an IR receiver object
 IRrecv irrecv(RECV_PIN);
 
 
-
 void setup() {
 
   //Testing
-  //Serial.begin(9600);
+  Serial.begin(9600);
 
   // Initialize pins
   pinMode(IN1, OUTPUT);
@@ -58,11 +66,18 @@ void setup() {
   pinMode(ENA, OUTPUT);
   pinMode(ENB, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  // Ensure the trigger pin is LOW initially
+  digitalWrite(TRIG_PIN, LOW);
+  delay(1000); // Give ultrasonic sensor some time to settle
 
   
   // Start L298N motor driver
   digitalWrite(ENA, HIGH);
   digitalWrite(ENB, HIGH);
+
 
   // CHECK IF NECCESSARY
   digitalWrite(LED_PIN, HIGH);
@@ -99,7 +114,12 @@ void loop() {
       driftLeft();
     }else if(decoded == REMOTE_NUM_9){
       driftRight();
+    }else if(decoded == STOP){
+      stop();
     }
+
+    // Do not let car go backward in case the distance to obstacle is less than DIST_THRESHOLD cm
+    if (checkDistance() < DIST_THRESHOLD and decoded == REMOTE_NUM_8) stop();
 
     // Update LED brightness depending on the photoresistor's measured voltage
     // if (millis() - lastTick >= 3000) {
@@ -117,12 +137,10 @@ void loop() {
 
     // Re-enable the IR receiver
     irrecv.resume();
-  }else{
-    delay(TICK);
-
-    // stop();
   }
-  //delay(TICK);
+
+  // Delay is required in order to make the car feel responsive to commands
+  delay(TICK);
 }
 
 void stop(){
@@ -149,14 +167,14 @@ void driftLeft(){
 
   // Enable only right wheels and spin forward
   analogWrite(ENA, 0);
-  analogWrite(ENB, 255);
+  analogWrite(ENB, 127);
   spinForward();
 }
 
 void driftRight(){
 
   // Enable only left wheels and spin forward
-  analogWrite(ENA, 255);
+  analogWrite(ENA, 127);
   analogWrite(ENB, 0);
   spinForward();
 }
@@ -164,8 +182,8 @@ void driftRight(){
 void backward(){
 
   // Enable all motors
-  analogWrite(ENA, 255);
-  analogWrite(ENB, 255);
+  analogWrite(ENA, 127);
+  analogWrite(ENB, 127);
   // Spin all wheels backward
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
@@ -176,16 +194,16 @@ void backward(){
 void forward(){
 
   // Enable all motors and spin forward
-  analogWrite(ENA, 255);
-  analogWrite(ENB, 255);
+  analogWrite(ENA, 127);
+  analogWrite(ENB, 127);
   spinForward();
 }
 
 void left(){
 
   //Fully enable right wheels and partially enable left wheels, then spin forward
-  analogWrite(ENA, 127);
-  analogWrite(ENB, 255);
+  analogWrite(ENA, 96);
+  analogWrite(ENB, 127);
   spinForward();
 }
 
@@ -193,6 +211,37 @@ void right(){
 
   //Fully enable left wheels and partially enable right wheels, then spin forward
   analogWrite(ENB, 96);
-  analogWrite(ENA, 255);
+  analogWrite(ENA, 127);
   spinForward();
+}
+
+float checkDistance(){
+  // Clear the TRIG_PIN
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+
+  // Send a 10 microsecond HIGH pulse to trigger the ultrasonic burst
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // Measure the time it takes for the echo to return
+  long duration = pulseIn(ECHO_PIN, HIGH, 30000); // Timeout after 30 ms (max ~5 meters)
+
+  // Check if a valid pulse was received
+  if (duration == 0) {
+    Serial.println("Warning: No echo received");
+    return -1;
+  }else{
+
+    // Calculate the distance in centimeters
+    float distance = duration / 58.3;
+
+    // Print the distance to the Serial Monitor
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+
+    return distance;
+  }
 }
